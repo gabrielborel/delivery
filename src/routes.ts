a3f7b804b051dabf902fa98bce44b8ecf61a6566
Gabriel Borel <biel_borel@hotmail.com>
2022-10-09 20:27:03 -0300

    CreateDeliveryman route created

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/AuthenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/CreateClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
-routes.post("/clients", createClientController.handle);
+routes.post("/client", createClientController.handle);
 routes.post("/authenticate", authenticateClientController.handle);
+
+routes.post("/deliveryman", createDeliverymanController.handle);
 
 export { routes };
